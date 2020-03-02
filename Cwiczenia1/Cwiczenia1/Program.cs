6c2ea1a6352f60f6e737cfa16dec1c9e3bd9957a
s18889 <s18889@LAB003-03>
2020-03-02 11:32:43 +0100

    dodalem projekt

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
